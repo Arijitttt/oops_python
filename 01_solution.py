8336a71435c49cda479dc713a0d98f4b01e56ba8
Arijit Bhattacharya <113541100+Arijitttt@users.noreply.github.com>
2024-08-22 20:44:35 +0530

    problem 1

---
+++ 01_solution.py
@@ -0,0 +1,13 @@
+#create a Car class with attributes like brand and model. Then create an instance of this class.
+
+class Car:
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+
+my_car = Car("Toyota","BMW")
+print(my_car.brand,my_car.model)
+
+
+my_new_car = Car("Tata","Safari")
+print(my_new_car.model)
